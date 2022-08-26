b2e2a0fa1867b6dd38764049743d095995703061
Nikolay <44751173+nklyy@users.noreply.github.com>
2022-08-26 11:30:09 +0300

    update BlockchainInfo model

--- src/api/bitcoin/model.rs
+++ src/api/bitcoin/model.rs
@@ -2,6 +2,44 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 #[serde(rename_all = "snake_case")]
-pub struct NodeInfo {
-    pub chain: String,
+struct Segwit {
+    #[serde(rename = "type")]
+    Type: String,
+    active: bool,
+    height: usize,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "snake_case")]
+struct Taproot {
+    #[serde(rename = "type")]
+    Type: String,
+    active: bool,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "snake_case")]
+struct Softforks {
+    segwit: Segwit,
+    taproot: Taproot,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct BlockchainInfo {
+    chain: String,
+    blocks: usize,
+    headers: usize,
+    bestblockhash: String,
+    difficulty: usize,
+    mediantime: usize,
+    verificationprogress: usize,
+    initialblockdownload: bool,
+    chainwork: String,
+    size_on_disk: usize,
+    pruned: bool,
+    pruneheight: usize,
+    automatic_pruning: bool,
+    prune_target_size: usize,
+    softforks: Softforks,
 }
